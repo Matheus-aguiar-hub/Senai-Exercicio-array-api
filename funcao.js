1446b60aa7b56f14e87909a2f5135e0a80f7f8bd
MATHEUS PEREIRA DE AGUIAR <25204557@cfp127aluno.local>
2026-03-18 11:28:03 -0300

    Estruturando a repetição

---
+++ funcao.js
@@ -0,0 +1,30 @@
+/* 
+Autor: Matheus Aguiar
+Objetivo: Criar funções para as respectivas chamadas.
+Deve retornar duas tratativas se for true retorna os dados ou se não retornar deve criar uma tratativa para false.
+App.js deve estar vazio, sem nenhum conteúdo.
+Data: 08/03/2026
+Versão 1.0
+*/
+
+const importandoEstados = require('./estados_cidades.js')
+
+const estado = []
+
+function getListaDeEstados (listaDeEstados) {
+
+    //Fazendo o import da sigla
+    let siglas = importandoEstados.listaDeEstados.estados
+
+    console.log(importandoEstados.listaDeEstados.estados.si)
+
+    //Estrutura de repetição
+    importandoEstados.listaDeEstados.estados.forEach(function(retornoSiglaEstado){
+
+
+
+    })
+
+} 
+
+getListaDeEstados('SP')
